06c211ba69886d8dda6f0efcbb971f18ca31ed68
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-03-26 16:32:07 +0530

    Create 9p.c

---
+++ 9p.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+int l,u,i,j,count=0,cout=0;
+clrscr();
+scanf("%d %d",&l,&u);
+for(i=l+1;i<u;i++)
+{
+
+
+for(j=1;j<=i;j++)
+
+{
+
+if(i%j==0)
+
+{
+
+count++;
+
+}
+
+}
+    if(count==2)
+    {
+    cout++;
+    }
+    count=0;
+}
+printf("\n%d",cout);
+getch();
+}
